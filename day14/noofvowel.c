15165bbf0c9cef7daf538d479d818bf4bdd605ac
EvaanikaAnandakumar <evaanikaanandakumar05@gmail.com>
2025-05-27 15:53:20 +0530

    day14

---
+++ day14/noofvowel.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<ctype.h>
+int main()
+{
+    char str[100];
+    int vowel=0;
+    int consonants=0;
+   
+    printf("enter the alphabet:");
+    fgets(str,100,stdin);
+    for(int i=0;str[i]!='\0';i++)
+    {
+        int ch=tolower(str[i]);
+        if(isalpha(ch))
+    {
+        if(ch=='a'|| ch=='A'|| ch=='e'|| ch=='E'|| ch =='I'||ch=='I'||ch=='o'||ch=='O'||ch=='u'||ch=='U')
+        {
+            vowel++;
+        }
+        else
+        {
+          consonants++;
+        }
+    }
+    }
+printf("vowels:%d\n",vowel);
+printf("consonsnt:%d\n",consonants);
+return 0;
+}
